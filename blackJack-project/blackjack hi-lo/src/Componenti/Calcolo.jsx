c4f8a77223a8797a955e7d41bb027b1afadaa565
Marco <Filanninobusiness@gmail.com>
2024-12-06 23:06:40 +0100

    Aggiunto Calcolo

---
+++ blackJack-project/blackjack hi-lo/src/Componenti/Calcolo.jsx
@@ -0,0 +1,10 @@
+export default function Calcolo() {
+  return (
+    <div>
+      <p>Azione da compiere: </p>
+      <p>Conteggio: </p>
+
+      <p>Se il punteggio è maggiore di 0: </p>
+    </div>
+  );
+}
